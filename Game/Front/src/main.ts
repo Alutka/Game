0a850bbc5574b95135b295939859c0b8ac92fee6
Alicja Krzeszowiec <alicja.krzeszowiec@vistex.com>
2020-04-01 16:43:42 +0200

    cors fix

--- Game/Front/src/main.ts
+++ Game/Front/src/main.ts
@@ -1,4 +1,7 @@
 console.log("main");
-fetch("api/characters").then(a => {
-    console.log(a.text())
-});
+fetch("api/characters",
+    {
+        mode: "no-cors"
+    }).then(a => {
+        console.log(a.text())
+    });
