f516349b7fd22fec20a09715c51a4d93d9367ab2
Alicja Krzeszowiec <alicja.krzeszowiec@vistex.com>
2020-04-01 12:42:32 +0200

    front added

---
+++ Game Front/src/info/info.ts
@@ -0,0 +1,5 @@
+const infoContainer = document.getElementById("info");
+const nameContainer = document.createElement("div");
+nameContainer.id = "name_container";
+infoContainer.appendChild(nameContainer);
+nameContainer.innerText = "Name: Batil";
---
+++ wwwroot/dist/index.js
@@ -0,0 +1,9 @@
+console.log("index log");
+console.log("main");
+console.log("actions");
+var infoContainer = document.getElementById("info");
+var nameContainer = document.createElement("div");
+nameContainer.id = "name_container";
+infoContainer.appendChild(nameContainer);
+nameContainer.innerText = "Name: Batil";
+//# sourceMappingURL=index.js.map
